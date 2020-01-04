2ce9381f709505e46b2331706b857a0071780f0a
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-03 20:04:51 -0500

    save

--- server/functions/makeDeck.js
+++ server/functions/makeDeck.js
@@ -1,6 +1,9 @@
 import { cardRanks, suits } from "../../shared/types";
 import { Card } from "../../shared/Card";
 
+/**
+ * @return {Card[][]}
+ */
 function makeDeck() {
   return Object.keys(cardRanks).map(function(cardRank) {
     return Object.keys(suits).map(function(suit) {
