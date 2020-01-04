d2b8f370b92759952b059280506819589bf15241
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-03 23:02:24 -0500

    save

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "tachyons/css/tachyons.min.css";
+
+import { cards } from "../../../shared/cards";
+import { cardRanks, suits } from "../../../shared/types";
+import * as cardRankUnicode from "../../../shared/cardRankUnicode";
 
 class Dashboard extends React.Component {
-  state = {};
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
+        <h1>{cardRankUnicode.diamonds.EIGHT}</h1>
       </div>
     );
   }
