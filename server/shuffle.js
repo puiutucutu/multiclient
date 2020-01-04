56a65c0d073c77b24f43b34f829230c7de2cbd71
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-03 19:06:25 -0500

    save

--- server/shuffle.js
+++ server/shuffle.js
@@ -1,17 +1,21 @@
-function shuffle(array) {
-  let counter = array.length;
+/**
+ * @param {Array} xs
+ * @return {Array}
+ */
+function shuffle(xs) {
+  let counter = xs.length;
   while (counter > 0) {
     // while there are elements in the array
     let index = Math.floor(Math.random() * counter); // pick a random index
     counter--; // decrease counter by 1
 
     // swap the last element with it
-    let temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
+    let temp = xs[counter];
+    xs[counter] = xs[index];
+    xs[index] = temp;
   }
 
-  return array;
+  return xs;
 }
 
 export { shuffle };
