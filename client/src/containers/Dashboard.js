f538c510c7f03613716fcb2c48173111104d224b
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-03 22:57:58 -0500

    save

---
+++ client/src/containers/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Dashboard extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export { Dashboard };
