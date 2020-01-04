790933f9d29ab010015d1e0a550e41f495c2c4f7
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-03 23:05:24 -0500

    save

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -6,11 +6,20 @@ import { cardRanks, suits } from "../../../shared/types";
 import * as cardRankUnicode from "../../../shared/cardRankUnicode";
 
 class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onBetClick(e) {
+    console.log("onBetClick");
+  }
+
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
-        <h1>{cardRankUnicode.diamonds.EIGHT}</h1>
+        <h1 className={"f1 b red"}>{cardRankUnicode.diamonds.EIGHT}</h1>
+        <button onClick={this.onBetClick}>Bet Amount</button>
       </div>
     );
   }
