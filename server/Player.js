bc0592d66f948832ec7b3cc4993448017e366ed1
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-04 13:42:51 -0500

    save

--- server/Player.js
+++ server/Player.js
@@ -1,3 +1,29 @@
+import { cardRanks } from "../shared/types"
+import { TWO } from '../shared/types/cardRanks'
+
+/**
+ * @param {CardRank} cardRank
+ * @return {number}
+ */
+function getCardValueFromCardRank(cardRank) {
+  const cardRankToValueDict = {
+    [cardRanks.ACE]: 2,
+    [cardRanks.TWO]: 2,
+    [cardRanks.THREE]: 3,
+    [cardRanks.FOUR]: 4,
+    [cardRanks.FIVE]: 5,
+    [cardRanks.SIX]: 6,
+    [cardRanks.SEVEN]: 7,
+    [cardRanks.EIGHT]: 8,
+    [cardRanks.NINE]: 9,
+    [cardRanks.TEN]: 10,
+    [cardRanks.JACK]: 10,
+    [cardRanks.QUEEN]: 10,
+    [cardRanks.KING]: 10,
+  }
+
+}
+
 class Player {
   /**
    * @type {Card[]}
@@ -24,6 +50,19 @@ class Player {
    */
   addCardToHand(card) {
     this.hand = [...this.hand, card];
+  }
+
+  getHandValue() {
+    return this.hand.reduce(function (total, card) {
+      const cardType = card.cardType;
+
+      switch () {
+
+      }
+
+      return total + card.cardType;
+
+    }, 0)
   }
 
   resetHand() {
