dfcb72d0dcfab10635ceaca4bc6dcbcf5ffc2744
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-04 13:14:58 -0500

    save

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -30,13 +30,31 @@ class Dashboard extends React.Component {
     console.log("onBetClick");
   }
 
+  onHit(e) {
+    console.log("onHit");
+  }
+
+  onStand(e) {
+    console.log("onStand");
+  }
+
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
-        <h1 className={"f1 b red"}>{cardRankUnicode.diamonds.EIGHT}</h1>
-        <button onClick={this.onBetClick}>Bet Amount</button>
-        <button onClick={this.onBetClick}>Deal</button>
+        <h1 className="f1 b red">{cardRankUnicode.diamonds.EIGHT}</h1>
+
+        <div className="db pa2 mb2 ba b--black br2 ba-light-grey">
+          <span className="db b f5">Current Hand</span>
+          <span className="db">KQ</span>
+        </div>
+
+        <div className="db pa2 ba b--black">
+          <button onClick={this.onBetClick}>Bet</button>
+          <button onClick={this.onBetClick}>Deal</button>
+          <button onClick={this.onHit}>Hit</button>
+          <button onClick={this.onStand}>Stand</button>
+        </div>
       </div>
     );
   }
