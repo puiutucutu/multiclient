92c70f9c23cad81778195010ea58b4a85309c48e
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-03 23:23:03 -0500

    save

--- server/GameKernel.js
+++ server/GameKernel.js
@@ -1,7 +1,15 @@
+import { makeDeck } from "./functions";
+
 class GameKernel {
   constructor(settings) {
     this.settings = settings;
   }
+
+  init() {
+    this.deck = makeDeck();
+  }
+
+  addPlayer(playerName, buyInAmount) {}
 }
 
 export { GameKernel };
