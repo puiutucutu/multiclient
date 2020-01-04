da47db44c0da322289a2661e41967efb4721991a
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-03 19:32:07 -0500

    save

---
+++ shared/cardRankUnicode.js
@@ -0,0 +1,15 @@
+const hearts = {
+  ACE: "\u{1F0B1}",
+  TWO: "\u{1F0B2}",
+  THREE: "\u{1F0B3}",
+  FOUR: "\u{1F0B4}",
+  FIVE: "\u{1F0B5}",
+  SIX: "\u{1F0B6}",
+  SEVEN: "\u{1F0B7}",
+  EIGHT: "\u{1F0B8}",
+  NINE: "\u{1F0B9}",
+  TEN: "\u{1F0BA}",
+  JACK: "\u{1F0BB}",
+  QUEEN: "\u{1F0BD}",
+  KING: "\u{1F0BE}"
+};
