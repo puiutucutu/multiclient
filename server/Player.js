12de02ab74d21e95f486be9f256ba15ed3b67384
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-04 15:01:20 -0500

    save

--- server/Player.js
+++ server/Player.js
@@ -52,32 +52,36 @@ class Player {
  * Handle card value resolving in such cases where the user has multiple aces.
  */
 function cardValueResolver() {
-  const hand = [
-    cards["CLUBS"].ace,
-    cards["CLUBS"].ace,
-    cards["CLUBS"].ace,
-    cards["CLUBS"].ace,
-    cards["CLUBS"].ace
-  ];
+  const hand = [cards["CLUBS"].ace, cards["CLUBS"].ace, cards["CLUBS"].ten];
 
   // prepare for cartesian
 
-  const handPreparedForCartesian = hand.map(function(card) {
-    return getCardValueFromCardRank(card.getRank());
-    // if (card.rank === cardRanks.ACE) {
-    //   return [1,11];
-    // } else {
-    //   return
-    // }
-  });
+  const handPreparedForCartesian = hand.map(card =>
+    getCardValueFromCardRank(card.getRank())
+  );
 
-  const possibleHandValues = findHandValuePermutations(hand);
+  // const possibleHandValues = cartesianProduct(
+  //   [handPreparedForCartesian]
+  // );
+
+  const possibleHandValues = cartesianProduct(handPreparedForCartesian);
 
   console.log(
     "%c handPreparedForCartesian",
     "background: red; color: white; font-weight: bold",
     handPreparedForCartesian
   );
+  console.log(
+    "%c possibleHandValues",
+    "background: red; color: white; font-weight: bold",
+    possibleHandValues
+  );
+  console.log(
+    "%c hand",
+    "background: red; color: white; font-weight: bold",
+    hand
+  );
+
   console.log(hand);
 }
 
@@ -90,33 +94,11 @@ function findHandValuePermutations(hand) {
 }
 
 /**
- * @param {array} head
- * @param {array} tail
- * @return {Generator<*[], void, ?>}
- * @example
- *
- * const a  = ['1','11'];
- * const b  = ['1','11'];
- * const c = ['10'];
- *
- * console.log(...cartesian(a, b, c));
- *
- */
-function* cartesian(head, ...tail) {
-  let remainder = tail.length ? cartesian(...tail) : [[]];
-  for (let r of remainder) {
-    for (let h of head) {
-      yield [h, ...r];
-    }
-  }
-}
-
-/**
  * @param {array} xs
  * @return {array}
  * @example
  *
- * cartesianProduct([["1", "11"], ["1", "11"], ["10"]]);
+ * cartesianProduct([["1", "11"], ["1", "11"], ["10"]]); //=> [[1,1,10],[1,11,10],[11,1,10],[11,11,10]]
  */
 function cartesianProduct(xs) {
   return xs.reduce(function(a, b) {
@@ -131,18 +113,18 @@ function cartesianProduct(xs) {
 function getCardValueFromCardRank(cardRank) {
   const cardRankToValueDict = {
     [cardRanks.ACE]: [1, 11],
-    [cardRanks.TWO]: 2,
-    [cardRanks.THREE]: 3,
-    [cardRanks.FOUR]: 4,
-    [cardRanks.FIVE]: 5,
-    [cardRanks.SIX]: 6,
-    [cardRanks.SEVEN]: 7,
-    [cardRanks.EIGHT]: 8,
-    [cardRanks.NINE]: 9,
-    [cardRanks.TEN]: 10,
-    [cardRanks.JACK]: 10,
-    [cardRanks.QUEEN]: 10,
-    [cardRanks.KING]: 10
+    [cardRanks.TWO]: [2],
+    [cardRanks.THREE]: [3],
+    [cardRanks.FOUR]: [4],
+    [cardRanks.FIVE]: [5],
+    [cardRanks.SIX]: [6],
+    [cardRanks.SEVEN]: [7],
+    [cardRanks.EIGHT]: [8],
+    [cardRanks.NINE]: [9],
+    [cardRanks.TEN]: [10],
+    [cardRanks.JACK]: [10],
+    [cardRanks.QUEEN]: [10],
+    [cardRanks.KING]: [10]
   };
 
   return cardRankToValueDict[cardRank];
