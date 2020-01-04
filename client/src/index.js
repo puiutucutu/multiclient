be0a6cb04621ddb054eb1eebc15ff90d14744394
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-03 22:59:07 -0500

    save

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Dashboard } from "./containers";
 
-class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
-}
+const mountNode = document.getElementById("app");
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="John Smith" />, mountNode);
+ReactDOM.render(<Dashboard />, mountNode);
