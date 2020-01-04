5af697f074b71c11cf73ed6efb071f1bb1199c2f
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-04 17:07:37 -0500

    Update Player.js

--- server/Player.js
+++ server/Player.js
@@ -98,8 +98,7 @@ function findHandValuePermutations(hand) {
  * @see https://stackoverflow.com/a/4331218
  * @example
  *
- * cartesianProduct([["1", "11"], ["1", "11"], ["10"]]); //=>
- *   [[1,1,10],[1,11,10],[11,1,10],[11,11,10]]
+ * cartesianProduct([["1", "11"], ["1", "11"], ["10"]]); //=> [[1,1,10],[1,11,10],[11,1,10],[11,11,10]]
  */
 function cartesianProduct(xs) {
   return xs.reduce(function(a, b) {
