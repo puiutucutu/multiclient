8744daced55d47e1c0c6eba657e5752f070464a6
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-03 23:26:06 -0500

    save

--- server/GameKernel.js
+++ server/GameKernel.js
@@ -10,6 +10,24 @@ class GameKernel {
   }
 
   addPlayer(playerName, buyInAmount) {}
+
+  isCardsInDeck() {
+    return this.currentDeck.length > 0;
+  }
+
+  getCardsRemaining() {
+    return this.currentDeck.length;
+  }
+
+  drawCard() {
+    if (!this.isCardsInDeck()) {
+      throw new Error("No more cards remain to be drawn from");
+    }
+
+    const [x, ...xs] = this.currentDeck;
+    this.currentDeck = xs;
+    return x;
+  }
 }
 
 export { GameKernel };
