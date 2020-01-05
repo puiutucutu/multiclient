5c57f8cda50cf79d9246cf3cb3077692fe4c42e5
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-05 15:53:25 -0500

    save

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,7 +1,8 @@
+import { cartesianProduct } from "./cartesianProduct";
 import { head } from "./head";
 import { makeDeck } from "./makeDeck";
 import { makeShoe } from "./makeShoe";
 import { shuffle } from "./shuffle";
 import { uniqueId } from "./uniqueId";
 
-export { head, makeDeck, makeShoe, shuffle, uniqueId };
+export { cartesianProduct, head, makeDeck, makeShoe, shuffle, uniqueId };
