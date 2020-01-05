6d6462b6391b250180899e185ef53375fcbbd29f
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-05 16:33:42 -0500

    save

--- server/Hand.js
+++ server/Hand.js
@@ -1,3 +1,15 @@
-class Hand {}
+class Hand {
+  /**
+   * @type {number}
+   */
+  bet;
+
+  /**
+   * @param {number} bet
+   */
+  constructor(bet) {
+    this.bet = bet;
+  }
+}
 
 export { Hand };
