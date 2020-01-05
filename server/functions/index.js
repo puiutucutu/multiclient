489d7cbc65c412ae4af16b71b6a3b7399eef066a
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-05 15:55:40 -0500

    save

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,6 +1,8 @@
 import { cartesianProduct } from "./cartesianProduct";
 import { getCardValueFromCardRank } from "./getCardValueFromCardRank";
 import { head } from "./head";
+import { isHandValueBlackjack } from "./isHandValueBlackjack";
+import { isHandValueLegal } from "./isHandValueLegal";
 import { makeDeck } from "./makeDeck";
 import { makeShoe } from "./makeShoe";
 import { shuffle } from "./shuffle";
@@ -10,6 +12,8 @@ export {
   cartesianProduct,
   getCardValueFromCardRank,
   head,
+  isHandValueBlackjack,
+  isHandValueLegal,
   makeDeck,
   makeShoe,
   shuffle,
